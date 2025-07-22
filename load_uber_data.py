97e974cf5eef63cefc1968d62aed78beb8ae6168
YUwayoOlga <uwayoolga@gmail.com>
2025-07-22 20:19:24 +0200

    EDA

--- load_uber_data.py
+++ load_uber_data.py
@@ -1,3 +1,10 @@
+#Loading data
 import pandas as pd
 df = pd.read_csv("uber_fares_dataset.csv") 
-print(df.head())
+df.head()
+# Understanding the data (EDA)
+print("Shape:", df.shape)
+df.info()
+print(df.describe())
+print("Missing values:\n", df.isnull().sum())
+print("Duplicate rows:", df.duplicated().sum())
